1597fc221bff5253088d4e1000b5901f0419b9cf
Jon Olds <jonolds@gmail.com>
2018-07-19 21:15:41 -0500

    after deleting paramControl

--- paramsControl.cpp
+++
@@ -1,9 +0,0 @@
-#include "paramsControl.hpp"
-#include <iostream>
-using namespace std;
-using namespace cv;
-using namespace calib;
-
-InternalParams ParamsControl::getInternalParameters() const {
-	return mIntParams;
-}
